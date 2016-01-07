c1605ac57f1005d10a159a28997484a23cb3e6c6
Harry-L <harryliu0@hotmail.com>
2016-01-07 14:47:54 -0500

    Project6a

---
+++ Project6a/Project2/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Harry Liu on 2016-01-05.
+//  Copyright © 2016 HarryLiu. All rights reserved.
+//
+
+import GameplayKit
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGrayColor().CGColor
+        button2.layer.borderColor = UIColor.lightGrayColor().CGColor
+        button3.layer.borderColor = UIColor.lightGrayColor().CGColor
+        
+        askQuestion()
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(countries) as! [String]
+        
+        button1.setImage(UIImage(named:countries[0]), forState: .Normal)
+        button2.setImage(UIImage(named:countries[1]), forState: .Normal)
+        button3.setImage(UIImage(named:countries[2]), forState: .Normal)
+        
+        correctAnswer = GKRandomSource.sharedRandom().nextIntWithUpperBound(3)
+        title = countries[correctAnswer].uppercaseString
+    }
+
+    @IBAction func buttonTapped(sender: UIButton) {
+        
+        if sender.tag == correctAnswer {
+            title = "Correct!"
+            score++
+        } else {
+            title = "Wrong!"
+            score--
+        }
+        
+        let mytext = "Your score is \(score)."
+        let ac = UIAlertController(title: title, message: mytext, preferredStyle: .Alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .Default, handler: askQuestion))
+        presentViewController(ac, animated: true, completion: nil)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
