e6b1267bd0b9cd2847a8f02fe8e63866aeda4f9d
Harry-L <harryliu0@hotmail.com>
2016-01-09 18:54:34 -0500

    Project10

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Harry Liu on 2016-01-09.
+//  Copyright © 2016 HarryLiu. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+
+}
