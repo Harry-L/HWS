42aeaa4e856a0e1dad054a7b4a72b07863ce8aa6
Harry-L <harryliu0@hotmail.com>
2016-01-12 12:47:05 -0500

    Project18

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Harry Liu on 2016-01-12.
+//  Copyright © 2016 HarryLiu. All rights reserved.
+//
+
+import UIKit
+import iAd
+
+class ViewController: UIViewController, ADBannerViewDelegate {
+    
+    var bannerView: ADBannerView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        bannerView = ADBannerView(adType: .Banner)
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        bannerView.delegate = self
+        bannerView.hidden = true
+        view.addSubview(bannerView)
+        
+        let viewsDictionary = ["bannerView": bannerView]
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[bannerView]|", options: [], metrics: nil, views: viewsDictionary))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[bannerView]|", options: [], metrics: nil, views: viewsDictionary))
+    }
+    
+    func bannerViewDidLoadAd(banner: ADBannerView!) {
+        bannerView.hidden = false
+    }
+    
+    func bannerView(banner: ADBannerView!, didFailToReceiveAdWithError error: NSError!) {
+        bannerView.hidden = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
